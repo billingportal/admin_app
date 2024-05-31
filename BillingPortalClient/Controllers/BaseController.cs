756105e7e8b17d7dcb825e90764d651746f813ba
billingportal <githubwork2023@gmail.com>
2024-05-31 15:50:43 +0500

    Update BaseController.cs

--- BillingPortalClient/Controllers/BaseController.cs
+++ BillingPortalClient/Controllers/BaseController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
 namespace BillingPortalClient.Controllers
@@ -8,8 +8,9 @@ namespace BillingPortalClient.Controllers
 
 
     protected string baseUrl = "https://billingportalapis.azurewebsites.net/";
-    Uri baseAddress = new Uri( "https://billingportalapis.azurewebsites.net/api/" );
-   //Uri baseAddress = new Uri( "http://localhost:7069/api/" );
+    protected Uri baseAddress = new Uri( "https://billingportalapis.azurewebsites.net/api/" );
+    // protected string baseUrl = "https://localhost:7069/";
+    // protected Uri baseAddress = new Uri( "https://localhost:7069/api/" );
     Uri oracleBaseAddress = new Uri( "http://88.85.242.29/api/" );
 
     protected readonly HttpClient _httpClient;
@@ -18,11 +19,11 @@ namespace BillingPortalClient.Controllers
     {
       _httpClient = new HttpClient();
       _httpClient.BaseAddress = baseAddress;
-      _httpClient.Timeout = TimeSpan.FromMinutes( 25 );
+      // _httpClient.Timeout = TimeSpan.FromMinutes( 25 );
 
       _httpClientOracle = new HttpClient();
       _httpClientOracle.BaseAddress = oracleBaseAddress;
-      _httpClientOracle.Timeout = TimeSpan.FromMinutes( 25 );
+      // _httpClientOracle.Timeout = TimeSpan.FromMinutes( 25 );
 
     }
   }
