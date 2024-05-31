547ffa69e95ad85927c19abbb5e4873353b76b6d
billingportal <githubwork2023@gmail.com>
2024-05-31 16:57:08 +0500

    Update Payment.cs

--- BillingPortalClient/Models/Payment.cs
+++ BillingPortalClient/Models/Payment.cs
@@ -1,33 +1,21 @@
-﻿namespace BillingPortalClient.Models
+namespace BillingPortalClient.Models
 {
   public partial class Payment
   {
     public int Id { get; set; }
-
     public int? InvoiceId { get; set; }
-
     public DateTime? PaymentDate { get; set; }
-
     public decimal? Amount { get; set; }
-
     public string? Status { get; set; }
-
     public string? PaymentMethod { get; set; }
-
+    public string? DocNumber { get; set; }
     public int? CustomerId { get; set; }
-
     public int? AccountId { get; set; }
-
     public string? AccountNumber { get; set; }
-
     public string? AccountName { get; set; }
-
     public virtual Account? Account { get; set; }
-
     public virtual Customer? Customer { get; set; }
-
     public virtual Invoice? Invoice { get; set; }
-
     public virtual ICollection<InvoicesPayment> InvoicesPayments { get; set; } = new List<InvoicesPayment>();
   }
 }
